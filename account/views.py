615f0c38de336e2892aaf4aa0fa897341e8151c9
Nishan Ahmed <nishan@Nishans-MacBook-Air.local>
2024-03-26 03:14:01 +0600

    modify render and generate tokens

--- account/views.py
+++ account/views.py
@@ -3,18 +3,33 @@ from rest_framework import status
 from django.contrib.auth import authenticate
 from rest_framework.views import APIView
 from account.serializers import UserRegistrationSerializer, UserLoginSerializer
+from account.renderers import UserRenderer
+from rest_framework_simplejwt.tokens import RefreshToken
 
+
+def get_tokens_for_user(user):
+    refresh = RefreshToken.for_user(user)
+
+    return {
+        'refresh': str(refresh),
+        'access': str(refresh.access_token),
+    }
 
 class UserRegistrationView(APIView):
+    renderer_classes = [UserRenderer]
+
     def post(self, request, format=None):
         serializer = UserRegistrationSerializer(data=request.data)
         if serializer.is_valid(raise_exception=True):
             user = serializer.save()
-            return Response({'message' : 'Registration Success'}, status=status.HTTP_201_CREATED)
+            token = get_tokens_for_user(user)
+            return Response({'token' : token, 'message' : 'Registration Success'}, status=status.HTTP_201_CREATED)
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
     
 
 class UserLoginView(APIView):
+    renderer_classes = [UserRenderer]
+
     def post(self, request, format=None):
         serializer = UserLoginSerializer(data=request.data)
         if serializer.is_valid(raise_exception=True):
@@ -23,6 +38,7 @@ class UserLoginView(APIView):
             user = authenticate(email=email, password=password)
 
             if user is not None:
-                return Response({'message' : 'Login Successfully'}, status=status.HTTP_200_OK)
+                token = get_tokens_for_user(user)
+                return Response({'token' : token, 'message' : 'Login Successfully'}, status=status.HTTP_200_OK)
             else:
                 return Response({'errors' : {'non_field_errors' : ['Email or Password is not Valid.']}}, status=status.HTTP_404_NOT_FOUND)
