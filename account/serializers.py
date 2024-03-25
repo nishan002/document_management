df46dae3e3625e054c01ea0344f5e9c907bb3d0d
Nishan Ahmed <nishan@Nishans-MacBook-Air.local>
2024-03-26 00:26:23 +0600

    restart project setup

---
+++ account/serializers.py
@@ -0,0 +1,30 @@
+from rest_framework import serializers
+from account.models import User
+
+
+class UserRegistrationSerializer(serializers.ModelSerializer):
+    confirm_password = serializers.CharField(style={'input_type' : 'password'}, write_only=True)
+    class Meta:
+        model = User
+        fields = ['email', 'name', 'password', 'confirm_password']
+        extra_kwargs = {
+            'password' : {'write_only' : True}
+        }
+
+    def validate(self, data):
+        password = data.get('password')
+        confirm_password = data.get('confirm_password')
+
+        if password != confirm_password:
+            raise serializers.ValidationError("Password and Confirm Password didn't matched.")
+        return data
+    
+    def create(self, validated_data):
+        return User.objects.create_user(**validated_data)
+    
+
+class UserLoginSerializer(serializers.ModelSerializer):
+    email = serializers.EmailField(max_length=255)
+    class Meta:
+        model = User
+        fields = ['email', 'password']
---
+++ account/views.py
@@ -0,0 +1,28 @@
+from rest_framework.response import Response
+from rest_framework import status
+from django.contrib.auth import authenticate
+from rest_framework.views import APIView
+from account.serializers import UserRegistrationSerializer, UserLoginSerializer
+
+
+class UserRegistrationView(APIView):
+    def post(self, request, format=None):
+        serializer = UserRegistrationSerializer(data=request.data)
+        if serializer.is_valid(raise_exception=True):
+            user = serializer.save()
+            return Response({'message' : 'Registration Success'}, status=status.HTTP_201_CREATED)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+    
+
+class UserLoginView(APIView):
+    def post(self, request, format=None):
+        serializer = UserLoginSerializer(data=request.data)
+        if serializer.is_valid(raise_exception=True):
+            email = serializer.data.get('email')
+            password = serializer.data.get('password')
+            user = authenticate(email=email, password=password)
+
+            if user is not None:
+                return Response({'message' : 'Login Successfully'}, status=status.HTTP_200_OK)
+            else:
+                return Response({'errors' : {'non_field_errors' : ['Email or Password is not Valid.']}}, status=status.HTTP_404_NOT_FOUND)
